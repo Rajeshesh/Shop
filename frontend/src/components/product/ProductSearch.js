66894571775f2bbac4e75fef2981894a86591942
rajesh <cisr6146@gmail.com>
2023-03-23 17:43:14 +0530

    re-slider by mui

--- frontend/src/components/product/ProductSearch.js
+++ frontend/src/components/product/ProductSearch.js
@@ -4,25 +4,22 @@ import { getProducts } from "../../actions/productActions";
 import Loader from ".././layouts/Loader";
 import MetaData from ".././layouts/MetaData";
 import Product from ".././product/Product";
-import  {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import Pagination from 'react-js-pagination';
 import { useParams } from "react-router-dom";
-import Slider from "rc-slider";
-import Tooltip from 'rc-tooltip';
-import 'rc-slider/assets/index.css';
-import 'rc-tooltip/assets/bootstrap.css';
+import RangeSlider from "./Slider";
 
-export  default function ProductSearch(){
+export default function ProductSearch() {
     const dispatch = useDispatch();
-    const {products, loading, error, productsCount, resPerPage} =    useSelector((state) => state.productsState)
+    const { products, loading, error, productsCount, resPerPage } = useSelector((state) => state.productsState)
     const [currentPage, setCurrentPage] = useState(1);
-    const [price, setPrice] = useState([1,1000]);
+    const [price, setPrice] = useState([0, 1000]);
     const [priceChanged, setPriceChanged] = useState(price);
     const [category, setCategory] = useState(null);
     const [rating, setRating] = useState(0);
 
     const { keyword } = useParams();
-    const categories = [  
+    const categories = [
         'Electronics',
         'Mobile Phones',
         'Laptops',
@@ -36,28 +33,28 @@ export  default function ProductSearch(){
         'Outdoor',
         'Home'
     ];
- 
-    const setCurrentPageNo = (pageNo) =>{
+
+    const setCurrentPageNo = (pageNo) => {
 
         setCurrentPage(pageNo)
-       
-    }
-    
 
-    useEffect(()=>{
-        if(error) {
-            return toast.error(error,{
+    }
+
+
+    useEffect(() => {
+        if (error) {
+            return toast.error(error, {
                 position: toast.POSITION.BOTTOM_CENTER
             })
         }
-        dispatch(getProducts(keyword, priceChanged, category, rating, currentPage)) 
-    }, [error, dispatch, currentPage, keyword, priceChanged, category, rating]) 
+        dispatch(getProducts(keyword, priceChanged, category, rating, currentPage))
+    }, [error, dispatch, currentPage, keyword, priceChanged, category, rating])
     // priceChanged instead of price
 
 
     return (
         <Fragment>
-            {loading ? <Loader/>:
+            {loading ? <Loader /> :
                 <Fragment>
                     <MetaData title={'Buy Best Products'} />
                     <h1 id="products_heading">Search Products</h1>
@@ -65,101 +62,78 @@ export  default function ProductSearch(){
                         <div className="row">
                             <div className="col-6 col-md-3 mb-5 mt-5">
                                 {/* Price Filter */}
-                                <div className="px-5" onMouseUp={()=>setPriceChanged(price)}>{/*note this onMouseUp */}
-                                    <Slider
-                                        range={true}
-                                        marks = {
-                                             {
-                                                1: "$1",
-                                                1000: "$1000"
-                                             }   
-                                        }
-                                        min={1}
-                                        max={1000}
-                                        defaultValue={price}
-                                        onChange={(price)=>{
-                                            setPrice(price)
-                                        }}
-                                        handleRender={
-                                            renderProps => {
-                                                return (
-                                                    <Tooltip  overlay={`$${renderProps.props['aria-valuenow']}`}  >
-                                                         <div {...renderProps.props}>  </div>
-                                                    </Tooltip>
-                                                )
-                                            }
-                                        }
-                                    />
+                                <div className="px-5" onMouseUp={() => setPriceChanged(price)}>
+                                    <RangeSlider price={price} setPrice={setPrice} />
                                 </div>
-                                <hr className="my-5" />        
+                                <hr className="my-5" />
                                 {/* Category Filter */}
                                 <div className="mt-5">
-                                     <h3 className="mb-3">Categories</h3> 
-                                       <ul className="pl-0">
+                                    <h3 className="mb-3">Categories</h3>
+                                    <ul className="pl-0">
                                         {categories.map(category =>
-                                             <li
-                                             style={{
-                                                 cursor:"pointer",
-                                                 listStyleType: "none"
-                                             }}
-                                             key={category}
-                                             onClick={()=>{
-                                                setCategory(category)
-                                             }}
-                                             >
-                                                 {category}
-                                             </li>
-                                            
-                                            )}
-                                           
-                                       </ul>
+                                            <li
+                                                style={{
+                                                    cursor: "pointer",
+                                                    listStyleType: "none"
+                                                }}
+                                                key={category}
+                                                onClick={() => {
+                                                    setCategory(category)
+                                                }}
+                                            >
+                                                {category}
+                                            </li>
+
+                                        )}
+
+                                    </ul>
                                 </div>
-                                <hr className="my-5" /> 
+                                <hr className="my-5" />
                                 {/* Ratings Filter */}
                                 <div className="mt-5">
                                     <h4 className="mb-3">Ratings</h4>
                                     <ul className="pl-0">
                                         {[5, 4, 3, 2, 1].map(star =>
-                                             <li
-                                             style={{
-                                                 cursor:"pointer",
-                                                 listStyleType: "none"
-                                             }}
-                                             key={star}
-                                             onClick={()=>{
-                                                setRating(star)
-                                             }}
-                                             >
-                                               <div className="ratings__outer">
-                                                    <div 
-                                                    className="ratings__inner"
-                                                    style={{
-                                                        width: `${star * 20}%`
-                                                    }}
-                                                    > 
+                                            <li
+                                                style={{
+                                                    cursor: "pointer",
+                                                    listStyleType: "none"
+                                                }}
+                                                key={star}
+                                                onClick={() => {
+                                                    setRating(star)
+                                                }}
+                                            >
+                                                <div className="ratings__outer">
+                                                    <div
+                                                        className="ratings__inner"
+                                                        style={{
+                                                            width: `${star * 20}%`
+                                                        }}
+                                                    >
 
                                                     </div>
-                                               </div>
-                                             </li>
-                                            
-                                            )}
-                                           
-                                       </ul>
+                                                </div>
+                                            </li>
+
+                                        )}
+
+                                    </ul>
                                 </div>
                             </div>
                             <div className="col-6 col-md-9">
                                 <div className="row">
-                                    { products && products.map(product => (
-                                        <Product col={4} key={product._id}  product={product}/>
+                                    {products && products.map(product => (
+                                        <Product col={4} key={product._id} product={product} />
                                     ))}
                                 </div>
 
                             </div>
                         </div>
                     </section>
-                    {productsCount > 0 && productsCount > resPerPage?
-                    <div className="d-flex justify-content-center mt-5">
-                           <Pagination 
+                    {productsCount > 0 && productsCount > resPerPage ?
+                        <div className="d-flex justify-content-center mt-5">
+                            <Pagination
                                 activePage={currentPage}
                                 onChange={setCurrentPageNo}
                                 totalItemsCount={productsCount}
@@ -169,10 +143,10 @@ export  default function ProductSearch(){
                                 lastPageText={'Last'}
                                 itemClass={'page-item'}
                                 linkClass={'page-link'}
-                           />     
-                    </div> : null }
+                            />
+                        </div> : null}
                 </Fragment>
-           }
+            }
         </Fragment>
     )
 }
---
+++ frontend/src/components/product/Slider.js
@@ -0,0 +1,34 @@
+import Slider from '@mui/material/Slider';
+
+export default function RangeSlider({ price: value, setPrice: setValue }) {
+    function valuetext(value) {
+        return `${value}`;
+    }
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Slider
+            min={10}
+            step={10}
+            max={1000}
+            getAriaLabel={() => 'Temperature range'}
+            value={value}
+            onChange={handleChange}
+            valueLabelDisplay="auto"
+            getAriaValueText={valuetext}
+            marks = {[
+                {
+                  value: 10,
+                  label: '10$',
+                },
+                {
+                  value: 1000,
+                  label: '1000$',
+                },
+              ]}
+        />
+    );
+}
