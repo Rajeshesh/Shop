354e7b9b0cb0c36cf55a73a5d1d978466073813f
rajesh <cisr6146@gmail.com>
2023-03-26 00:39:46 +0530

    orderDetail chang

--- frontend/src/components/order/OrderDetail.js
+++ frontend/src/components/order/OrderDetail.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { Link, useParams } from "react-router-dom";
 import { orderDetail as orderDetailAction } from '../../actions/orderAction'
 import Loader from "../layouts/Loader";
+import { Box } from '@mui/material'
 
 export default function OrderDetail() {
 
@@ -23,47 +24,50 @@ export default function OrderDetail() {
     return (
         <>
             {loading ? <Loader /> : <>
-                <div className="row d-flex justify-content-between">
-                    <div className="col-12 col-lg-8 mt-5 order-details">
+                <div className="">
+                    <div className="orderDetail">
 
-                        <h1 className="my-5">Order # {orderDetail._id}</h1>
-
-                        <h4 className="mb-4">Shipping Info</h4>
-                        <p><b>Name:</b> {user.name}</p>
-                        <p><b>Phone:</b> {shippingInfo.phoneNo}</p>
-                        <p className="mb-4"><b>Address:</b>{shippingInfo.address}, {shippingInfo.city} , {shippingInfo.state}, {shippingInfo.postalCode},{shippingInfo.country}</p>
-                        <p><b>Amount:</b> ${totalPrice}</p>
+                        <h4 className="mb-4 mt-4">Shipping Info</h4>
+                        <div className="pl-15">
+                            <p><b>Name:</b> {user.name}</p>
+                            <p><b>Phone:</b> {shippingInfo.phoneNo}</p>
+                            <p className="mb-4"><b>Address:</b>{shippingInfo.address}, {shippingInfo.city} , {shippingInfo.state}, {shippingInfo.postalCode},{shippingInfo.country}</p>
+                            <p><b>Amount:</b> ${totalPrice}</p>
+                        </div>
 
                         <hr />
 
-                        <h4 className="my-4">Payment</h4>
+                        <h4 className="mt-4 mb-2">Payment</h4>
                         <p className={isPaid ? 'greenColor' : 'redColor'}><b>{isPaid ? 'PAID' : 'NOT PAID'}</b></p>
 
 
-                        <h4 className="my-4">Order Status:</h4>
+                        <h4 className="mt-4 mb-2">Order Status</h4>
                         <p className={orderStatus && orderStatus.includes('Delivered') ? 'greenColor' : 'redColor'}><b>{orderStatus}</b></p>
 
 
-                        <h4 className="my-4">Order Items:</h4>
-
                         <hr />
-                        <div className="cart-item my-1">
+                        <h4 className="mt-4 mb-4">Order Items</h4>
+
+                        <div className=" mt-1 mb-1">
                             {orderItems && orderItems.map((item, i) => (
-                                <div className="row my-5" key={i}>
-                                    <div className="col-4 col-lg-2">
+                                <div className="orderItem mt-5 mb-5" key={i}>
+                                    <div className="">
                                         <img src={item.image} alt="Laptop" height="45" width="65" />
                                     </div>
 
-                                    <div className="col-5 col-lg-5">
+                                    <Box sx={{ display: { xs: 'none', sm: 'block' } }} className="">
                                         <Link to={`/product/${item.product}`}>{item.name}</Link>
-                                    </div>
+                                    </Box>
+                                    <Box sx={{ display: { xs: 'block', sm: 'none' } }} className="">
+                                        <Link to={`/product/${item.product}`}>{item.name.slice(0, 6)}...</Link>
+                                    </Box>
 
 
-                                    <div className="col-4 col-lg-2 mt-4 mt-lg-0">
+                                    <div className="mt-4">
                                         <p>${item.price}</p>
                                     </div>
 
-                                    <div className="col-4 col-lg-3 mt-4 mt-lg-0">
+                                    <div className="mt-4">
                                         <p>{item.quantity} Piece(s)</p>
                                     </div>
                                 </div>
